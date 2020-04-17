fe9beeedcfc2e3db4041d11c44bf5115a7a3c97c
RyanSample <sampleryan1@gmail.com>
2020-04-17 16:02:33 -0500

    Removed some settings from the settings.py module and removed a shebang from the manage.py module

--- ZwiftRoutePlanner/settings.py
+++ ZwiftRoutePlanner/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from . import utility
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y$09%cwe_wt5^9+ljd)3c-i&=r(-6=mdkna3$w6&6j#rgw#7zx'
+SECRET_KEY = utility.PROJECT_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
