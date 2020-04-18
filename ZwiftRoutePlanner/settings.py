dc70d82dfe347680727682cafb9882956e1f0468
RyanSample <sampleryan1@gmail.com>
2020-04-17 19:57:14 -0500

    Created a model for Worlds and Routes

--- ZwiftRoutePlanner/settings.py
+++ ZwiftRoutePlanner/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'routeplanner.apps.RouteplannerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- routeplanner/models.py
+++ routeplanner/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class World(models.Model):
+    world_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.world_name
+
+class Route(models.Model):
+    world = models.ForeignKey(World, on_delete=models.CASCADE)
+    route_name = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.world + ' ' + self.route_name
