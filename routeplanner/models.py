9d8f96acdc651bff67a72e64f2913f375ab18757
RyanSample <sampleryan1@gmail.com>
2020-05-04 20:58:24 -0500

    Updated the route model to add elevation gain

--- routeplanner/models.py
+++ routeplanner/models.py
@@ -10,6 +10,7 @@ class Route(models.Model):
     world = models.ForeignKey(World, on_delete=models.CASCADE)
     route_name = models.CharField(max_length=200)
     route_length = models.FloatField(default=0.0)
+    elevation_gain = models.IntegerField(default=0)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
