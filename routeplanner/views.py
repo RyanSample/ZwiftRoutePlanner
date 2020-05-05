ba9905db7dfc3ba2871fe080c04d0b32aeb94bb9
RyanSample <sampleryan1@gmail.com>
2020-05-04 20:40:35 -0500

    Added detail template, worked on the index view to group routes under their respective world, fixed amodel query issue for the details view

--- routeplanner/views.py
+++ routeplanner/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-from .models import Route
+from .models import Route, World
 
 def index(request):
     routes_list = Route.objects.order_by('-route_name')
+    worlds_list = World.objects.order_by('-world_name')
     context = {
+        'worlds_list': worlds_list,
         'routes_list': routes_list,
     }
     return render(request, 'routeplanner/index.html', context)
 
 def detail(request, route_name):
-    #route = get_object_or_404(Route, pk=route_name)
-    return HttpResponse("Route name: %s" % route_name)
+    try:
+        route = Route.objects.get(route_name__exact=route_name)
+    except Route.DoesNotExist:
+        raise Http404("Route does not exist...")
+
+    return render(request, 'routeplanner/detail.html', {'route': route})
 
